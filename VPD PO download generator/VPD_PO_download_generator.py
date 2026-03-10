f4c4cb66e4380dbbd5cf41101b197945ee24a5b9
jwa2565 <jwa2565@vt.edu>
2026-03-10 15:37:49 -0400

    initial commit
    Starting this thang

--- VPD PO download generator/VPD_PO_download_generator.py
+++ VPD PO download generator/VPD_PO_download_generator.py
@@ -1,1 +1,12 @@
+#version notes
+#BETA v0.1: Initial attempt
 
+print("Starting VPD's PO Download Generator!")
+print("Importing data...")
+
+import time
+from datetime import date
+today = str(date.today())
+
+import pandas as pd
+import os
